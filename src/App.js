8e6d024b810dd16c26261e471ebce19ee41247fc
shashank <sarthanur@gmail.com>
2023-04-23 11:49:49 +0530

    remove component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import Jokes from "./components/jokes/Jokes.jsx"
-import Cat from "./components/cat/Cat.jsx";
-import Dog from "./components/dog/Dog.jsx";
+
 
 function App() {
   return (
     <>
       <Jokes />
-      <br /><br /><br /><br />
-      <Cat />
-      <br /><br /><br /><br />
-      <Dog />
     </>
   );
 }
